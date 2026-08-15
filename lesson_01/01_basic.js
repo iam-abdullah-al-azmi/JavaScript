2f71a5ce32eb7db93ed0460c1ffe9dd2bce347c6
Azmi-84 <azmi@iut-dhaka.edu>
2026-08-15 12:09:27 +0600

    Completed first excersie of the basic JavaScript, e.g., how to make the web page blank?

--- lesson_01/01_basic.js
+++ lesson_01/01_basic.js
@@ -5,4 +5,8 @@
 // First open a web-browser and open the console.
 // Then, write the alert('Hello World!')
 // A new alert windows will pop up with the message 'Hello World!'
-//
+// Then, we can perform the math exercises in the console e.g., 2 + 2, or 10 - 5
+// The output will be 2 + 2 = 4, and 10 - 5 = 5
+// We can also use the document.body.innerHTML to change the content of the web-page
+// For example, document.body.innerHTML = 'Hello World!', will make the page only display Hello World!
+// For creating a blank page, we can use document.body.innerHTML = ''
