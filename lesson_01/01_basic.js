fcf9d9fc6789f9985e8131dad1dc8740aece2832
Azmi-84 <azmi@iut-dhaka.edu>
2026-08-15 12:03:14 +0600

    First basic lesson of JavaScript on how to use it in the web-browser e.g., Google Chrome, or Safari, or Microsoft Edge

---
+++ lesson_01/01_basic.js
@@ -0,0 +1,8 @@
+// The lesson of JavaScript is learning from the following resource: https://youtu.be/EerdGm-ehJQ?si=WrHq86wm_ZrFK3F_
+
+// This is the first basic lesson of JavaScript.
+
+// First open a web-browser and open the console.
+// Then, write the alert('Hello World!')
+// A new alert windows will pop up with the message 'Hello World!'
+//
