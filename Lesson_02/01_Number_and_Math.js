fda84b03f257a47ce97c19013622eb3988703e9e
Azmi-84 <azmi@iut-dhaka.edu>
2026-08-20 12:31:13 +0600

    Remaned files and folders using convention e.g., capitalize the first letter of the name. Then, learned the number and math opearation in JavaScript.

---
+++ Lesson_02/01_Number_and_Math.js
@@ -0,0 +1,40 @@
+// Integer Number
+
+// Addition
+console.log("Integer Addition", 2 + 2);
+
+// Subtraction
+console.log("Integer Subtraction", 2 - 2);
+
+// Multiplication
+console.log("Integer Multiplication", 2 * 2);
+
+// Division
+console.log("Integer Division", 2 / 2);
+
+// Modulus
+console.log("Integer Modulus", 2 % 2);
+
+// Float Number
+
+// Addition
+console.log("Float Addition", 2.5 + 2.2);
+
+// Subtraction
+console.log("Float Subtraction", 2.5 - 2.2);
+
+// Multiplication
+console.log("Float Multiplication", 2.5 * 2.2);
+
+// Division
+console.log("Float Division", 2.5 / 2.2);
+
+// Modulus
+console.log("Float Modulus", 2.5 % 2.2);
+
+// Precedence (Division > Multiplication > Subtraction > Addition)
+console.log("First Subtraction and Then Addition", 2 + 2 - 2);
+console.log("First Multiplication and Then Addition", 2 + 2 * 2);
+
+// Bracket Precedence
+console.log("First Operation in Bracket", (2 + 2.5 * 4.5) * 2);
